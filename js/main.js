bca52de5e8b9bfadf0e270554b10811b526e8ac1
Dan Villa <myebphotos@gmail.com>
2018-01-06 22:30:46 -0800

    updated game logic to use functions

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
 var cards = ["queen", "queen", "king", "king"]
 var cardsInPlay = [];
-var cardOne;
 
-cardOne = cards[0];
-cardsInPlay.push(cardOne);
-cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped "+cardOne);
-console.log("User flipped "+cardTwo);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	}
-	else {
-		alert("Sorry, try again.");
-	}
+		else {
+			alert("Sorry, try again.");
+		}
 }
-	else {
-		alert("Flip another card");
-	}
 
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+		}
+		else {
+		// Set this alert later...	alert("Flip another card");
+		}
+}
+
+flipCard(0);
+flipCard(2);
