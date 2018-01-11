7fe85bdf31e397cf9cf73bff6b8795e08d044604
Dan Villa <myebphotos@gmail.com>
2018-01-10 19:50:59 -0800

    added score counter and reset button

--- js/main.js
+++ js/main.js
@@ -10,6 +10,11 @@
 //          h2 element is controlled in this script.  This was done because
 //          the 2nd card that was selected would not get displayed until the
 //          OK button was clicked on the alert box and did not look very good.
+//
+//          10-Jan-2018:
+//          * Added logic to keep track of score or number of matches.
+//          * Added number of times the user has played.
+//          * Added logic for reset button.
 //*-------------------------------------------------------------------------*/
 
 // cards array objects
@@ -39,20 +44,46 @@ cardImage: 'images/king-of-diamonds.png'
 // This array holds the cards in play
 var cardsInPlay = [];
 
+// The Win and Play counts are stored at the browser session level.
+// User will have to close browser to reset counts.
+var winCount = sessionStorage.getItem("winCountS");
+var playCount = sessionStorage.getItem("playCountS");
+
+// This function is used for testing
+/* resetPlays(); */
+
+if (winCount===null) {
+	winCount=0;
+};
+if (playCount===null) {
+	playCount=0;
+};
+
 // This function checks if there is a match for the two cards in play
 function checkForMatch() {
+	console.log("In checkForMatch");
 	if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
 		// Replaced alert with the following h2 element.
 		document.getElementById('matchOrNot').textContent = 'MATCH FOUND!';	
 		//alert("You found a match!");
+		winCount++;
 	}
 	else {
 		// Replaced alert with the following h2 element.
 		document.getElementById('matchOrNot').textContent = 'Sorry, try again.';
 		//alert("Sorry, try again.");
 	}
-	// Display the match or no match message
+	
+	playCount++; // Increment the playCount
+	// Display the score, match or no match message and reset button
+	document.getElementsByClassName("score")[0].textContent = "You've matched " + winCount + " of " + playCount + " plays.";
+	document.getElementsByClassName("score")[0].style.visibility = "visible";
 	document.getElementById("matchOrNot").style.visibility = "visible";
+	document.getElementsByTagName("button")[0].style.visibility = "visible";
+
+	// Save the win/match and play counts to the browser session
+	sessionStorage.setItem("winCountS", winCount);
+	sessionStorage.setItem("playCountS", playCount);
 }
 
 // This function flips a card and puts the card in the cardsInPlay array.
@@ -66,7 +97,7 @@ function flipCard() {
 	}
 	// If user attempts to click another card, reload the page to start again.
 	else if (cardsInPlay.length === 3) {
-			location.reload();
+			resetPage();
 		}
 }
 
@@ -74,6 +105,7 @@ function flipCard() {
 function createBoard() {
 	// Initially hide this h2 element.  Only displays when checkForMatch is called.
 	document.getElementById("matchOrNot").style.visibility = "hidden";
+	document.getElementsByTagName("button")[0].style.visibility = "hidden";
 	for (var i=0; i<cards.length; i++) {
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute("src", "images/back.png");
@@ -81,6 +113,18 @@ function createBoard() {
 		cardElement.addEventListener("click",flipCard);
 		document.getElementById("game-board").appendChild(cardElement);
 	}
+}
+
+function resetPage() {
+	location.reload();
+}
+
+// Function used for testing.  Reset the counts in the browser session.
+function resetPlays() {
+	winCount = 0;
+	playCount = 0;
+	sessionStorage.setItem("winCountS", winCount);
+	sessionStorage.setItem("playCountS", playCount);
 }
 
 createBoard();
